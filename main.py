cfa2b8963e3e8e7728eaffdfcf35c582c888669c
sana.kusakabe <s.kusakabe@peach-tech.co.jp>
2025-03-20 11:50:11 +0900

    TEST

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import sqlite3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from google import genai
-from pydantic import BaseModel
+from app import models
 
-dbname = 'APItest.db'
+dbFile = 'test.db'
+# 1.データベースに接続
+con = sqlite3.connect(dbFile)
+
+# 2.sqliteを操作するカーソルオブジェクトを作成
+cur = con.cursor()
+
 app = FastAPI()
 
-conn=sqlite3.connect(dbname)
-c = conn.cursor()
+# Todoを登録　参考https://qiita.com/mtitg/items/47770e9a562dd150631d
+@app.post("/todos/")
+async def create_todo(todo_in: TodoIn,  db: Session = Depends(get_db)):
+    todo = Todo(title=todo_in.title, done=False)
+    db.add(todo)
+    db.commit()
+    todo = get_todo(db, todo.id)
+    return todo
 
-# executeメソッドでSQL文を実行する
-create_table = '''create table toukeilab (id value,name verchar)'''
-c.execute(create_table)
+# Todoを更新
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_in: TodoIn, db: Session = Depends(get_db)):
+    todo = get_todo(db, todo_id)
+    todo.title = todo_in.title
+    todo.done = todo_in.done
+    db.commit()
+    todo = get_todo(db, todo_id)
+    return todo
+
+
+"""
+# ユーザー登録（create）
+@app.post("/users", response_model=User)
+def create_user(request: UserCreate):
+    db_user = models.User(email=request.email, name=request.name)
+    session = models.SessionLocal()
+    try:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+        return db_user
+
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+
+"""
