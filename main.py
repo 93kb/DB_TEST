a3df78daa95c63703ed651c111ee2e05c8d6338c
sana kusakabe <s.kusakabe@peach-tech.co.jp>
2025-03-24 10:34:35 +0900

    TEST

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-import sqlite3
-from fastapi import FastAPI
-from google import genai
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from app import models
+from app.schemas import User, UserCreate
 
-dbname = 'APItest.db'
 app = FastAPI()
 
-conn=sqlite3.connect(dbname)
-c = conn.cursor()
 
-# executeメソッドでSQL文を実行する
-create_table = '''create table toukeilab (id value,name verchar)'''
-c.execute(create_table)
+# ユーザー登録（create）
+@app.post("/users", response_model=User)
+def create_user(request: UserCreate):
+    db_user = models.User(email=request.email, name=request.name)
+    session = models.SessionLocal()
+    try:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+        return db_user
+
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+
+
+# ユーザー情報読み込み（read）
+@app.get("/users")
+def read_user():
+    session = models.SessionLocal()
+    db_user = session.query(models.User).all()
+    return db_user
+
+
+# ユーザー情報更新（update）
+@app.patch("/users")
+def update_user():
+    session = models.SessionLocal()
+    db_user = session.User()
+    
+    
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+# ユーザー情報削除（delete）
+@app.delete("/users")
+def delete_user():
+    session = models.SessionLocal()
+    db_user = session.User()
+    
+    session.delete(db_user)
+    session.commit()
+    return db_user
